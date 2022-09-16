27cda507d2e65de4ffe0dd4601e21bd3198d76f3
AlexanderJernstrom <alexanderjernstrom@gmail.com>
2022-09-16 16:04:04 +0200

    Added React-router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {
+  BrowserRouter as Router,
+  RouterProvider,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
